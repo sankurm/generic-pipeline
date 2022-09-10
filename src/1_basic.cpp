5e5dcfc56c71034c54a8940512b4e8a606653aa3
sankurm <sankurm@yahoo.com>
2022-09-11 02:39:46 +0530

    Moved init_kafka inside anonymous namespace

--- src/1_basic.cpp
+++ src/1_basic.cpp
@@ -39,26 +39,26 @@ namespace
     std::optional<kafka_consumer> subscribe(kafka_consumer&& consumer) {
         return consumer.subscribe()? std::make_optional(consumer): std::nullopt;
     }
-}
 
-std::optional<kafka_consumer> init_kafka() {
-    auto fname = get_env("kafka-config-filename");
-    if (!fname) { return std::nullopt; }
+    std::optional<kafka_consumer> init_kafka() {
+        auto fname = get_env("kafka-config-filename");
+        if (!fname) { return std::nullopt; }
 
-    auto contents = get_file_contents(std::move(fname.value()));
-    if (!contents) { return std::nullopt; }
+        auto contents = get_file_contents(std::move(fname.value()));
+        if (!contents) { return std::nullopt; }
 
-    auto config = parse_kafka_config(std::move(contents.value()));
-    if (!config) { return std::nullopt; }
+        auto config = parse_kafka_config(std::move(contents.value()));
+        if (!config) { return std::nullopt; }
 
-    auto cons = create_kafka_consumer(std::move(config.value()));
-    if (!cons) { return std::nullopt; }
+        auto cons = create_kafka_consumer(std::move(config.value()));
+        if (!cons) { return std::nullopt; }
 
-    auto consumer = cons.value();
-    if (consumer.connect() && consumer.subscribe()) {
-        return cons;
+        auto consumer = cons.value();
+        if (consumer.connect() && consumer.subscribe()) {
+            return cons;
+        }
+        return std::nullopt;
     }
-    return std::nullopt;
 }
 
 int main(int argc, char** argv) {
