a423b6c388266afd52b92cedc674f66099a066be
sankurm <sankurm@yahoo.com>
2022-09-11 02:36:58 +0530

    Complete implementation without pipe

--- src/1_basic.cpp
+++ src/1_basic.cpp
@@ -13,12 +13,31 @@ namespace
         return "file-contents-blah-blah";
     }
 
+    struct kafka_config { /* url etc. */ };
+
+    std::optional<kafka_config> parse_kafka_config(std::string&& config) {
+        return std::make_optional(kafka_config{});
+    }
+
     struct kafka_consumer
     {
-        kafka_consumer(std::string&& config) {}
+        kafka_consumer(const kafka_config& config) {}
+        kafka_consumer(kafka_config&& config) {}
+
+        bool connect() { return true; }
+        bool subscribe() { return true; }
     };
-    std::optional<kafka_consumer> create_kafka_consumer(std::string&& config) {
-        return kafka_consumer(std::move(config));
+
+    std::optional<kafka_consumer> create_kafka_consumer(kafka_config&& config) {
+        return std::make_optional<kafka_consumer>(std::move(config));
+    }
+
+    std::optional<kafka_consumer> connect(kafka_consumer&& consumer) {
+        return consumer.connect()? std::make_optional(consumer): std::nullopt;
+    }
+
+    std::optional<kafka_consumer> subscribe(kafka_consumer&& consumer) {
+        return consumer.subscribe()? std::make_optional(consumer): std::nullopt;
     }
 }
 
@@ -26,10 +45,20 @@ std::optional<kafka_consumer> init_kafka() {
     auto fname = get_env("kafka-config-filename");
     if (!fname) { return std::nullopt; }
 
-    auto config = get_file_contents(std::move(fname.value()));
+    auto contents = get_file_contents(std::move(fname.value()));
+    if (!contents) { return std::nullopt; }
+
+    auto config = parse_kafka_config(std::move(contents.value()));
     if (!config) { return std::nullopt; }
 
-    return create_kafka_consumer(std::move(config.value()));
+    auto cons = create_kafka_consumer(std::move(config.value()));
+    if (!cons) { return std::nullopt; }
+
+    auto consumer = cons.value();
+    if (consumer.connect() && consumer.subscribe()) {
+        return cons;
+    }
+    return std::nullopt;
 }
 
 int main(int argc, char** argv) {
